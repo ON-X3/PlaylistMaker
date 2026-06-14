8ed740e770d3b826052197884c00a97a6b783be2
Nikolay <ordi-n-x3@yandex.ru>
2026-06-14 11:29:14 +0500

    Сдача на ревью (спринт 19)

---
+++ app/src/main/java/com/practicum/playlistmaker/player/ui/PlayerFragment.kt
@@ -0,0 +1,129 @@
+package com.practicum.playlistmaker.player.ui
+
+import android.content.Context
+import android.os.Bundle
+import android.util.TypedValue
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.practicum.playlistmaker.R
+import com.practicum.playlistmaker.databinding.FragmentPlayerBinding
+import com.practicum.playlistmaker.search.ui.models.TrackUi
+import org.koin.androidx.viewmodel.ext.android.getViewModel
+import org.koin.core.parameter.parametersOf
+
+class PlayerFragment : Fragment() {
+
+    private var _binding: FragmentPlayerBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var viewModel: PlayerViewModel
+
+    companion object {
+        private const val ARGS_TRACK = "track"
+
+        fun createArgs(track: TrackUi): Bundle = Bundle().apply {
+            putParcelable(ARGS_TRACK, track)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPlayerBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val track = requireArguments().getParcelable<TrackUi>(ARGS_TRACK)!!
+
+        viewModel = getViewModel { parametersOf(track.previewUrl) }
+
+
+        binding.playButton.isEnabled = false
+        binding.playButton.setOnClickListener {
+            viewModel.playbackControl()
+        }
+
+        viewModel.observePlayerState().observe(viewLifecycleOwner) {
+            changeButtonImage(it == PlayerViewModel.STATE_PLAYING)
+            enableButton(it != PlayerViewModel.STATE_DEFAULT)
+        }
+
+        viewModel.observePlayProgress().observe(viewLifecycleOwner) {
+            binding.playProgress.text = it
+        }
+
+        binding.toolbar.setNavigationOnClickListener {
+            findNavController().navigateUp()
+        }
+        bindTrackInfo(track)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        viewModel.onPause()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun dpToPx(dp: Float, context: Context): Int {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            dp,
+            context.resources.displayMetrics
+        ).toInt()
+    }
+
+    private fun changeButtonImage(isPlaying: Boolean) {
+        if (isPlaying) {
+            binding.playButton.setBackgroundResource(R.drawable.button_pause_100)
+        } else {
+            binding.playButton.setBackgroundResource(R.drawable.button_play_100)
+        }
+    }
+
+    private fun enableButton(isEnabled: Boolean) {
+        binding.playButton.isEnabled = isEnabled
+    }
+
+    private fun bindTrackInfo(track: TrackUi) {
+        Glide.with(this)
+            .load(track.getCoverArtwork())
+            .placeholder(R.drawable.placeholder_45)
+            .error(R.drawable.placeholder_45)
+            .transform(RoundedCorners(dpToPx(8f, requireContext())))
+            .into(binding.artwork)
+
+        binding.trackName.text = track.trackName
+        binding.artistName.text = track.artistName
+        binding.trackTimeValue.text = track.trackTime
+        binding.genreValue.text = track.primaryGenreName
+        binding.countryValue.text = track.country
+
+        if (track.collectionName != null) {
+            binding.albumValue.text = track.collectionName
+        } else {
+            binding.albumValue.visibility = View.GONE
+            binding.album.visibility = View.GONE
+        }
+
+        if (track.releaseDate != null) {
+            binding.releaseDateValue.text = track.releaseDate.substring(0, 4)
+        } else {
+            binding.releaseDateValue.visibility = View.GONE
+            binding.releaseDate.visibility = View.GONE
+        }
+    }
+}
---
+++ app/src/main/java/com/practicum/playlistmaker/search/ui/activity/SearchFragment.kt
@@ -0,0 +1,246 @@
+package com.practicum.playlistmaker.search.ui.activity
+
+import android.content.Context.INPUT_METHOD_SERVICE
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.practicum.playlistmaker.R
+import com.practicum.playlistmaker.databinding.FragmentSearchBinding
+import com.practicum.playlistmaker.player.ui.PlayerFragment
+import com.practicum.playlistmaker.search.ui.models.TrackUi
+import com.practicum.playlistmaker.search.ui.viewmodel.SearchState
+import com.practicum.playlistmaker.search.ui.viewmodel.SearchViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class SearchFragment : Fragment() {
+
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding!!
+
+    private var isClickAllowed = true
+    private lateinit var adapter: TrackAdapter
+    private lateinit var searchHistoryAdapter: TrackAdapter
+    private val viewModel: SearchViewModel by viewModel()
+    private lateinit var searchTextWatcher: TextWatcher
+    private val handler = Handler(Looper.getMainLooper())
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        adapter = TrackAdapter { track ->
+            onTrackClick(track)
+        }
+        searchHistoryAdapter = TrackAdapter { track ->
+            onTrackClick(track)
+        }
+
+        binding.searchHistoryList.adapter = searchHistoryAdapter
+
+        binding.tracksList.adapter = adapter
+
+        binding.updateButton.setOnClickListener {
+            viewModel.searchTracks(binding.searchEditText.text.toString())
+        }
+
+        binding.searchEditText.setText(viewModel.getLatestSearchText())
+
+        viewModel.observeState().observe(viewLifecycleOwner) {
+            render(it)
+        }
+
+        viewModel.observeShowToast().observe(viewLifecycleOwner) {
+            Toast.makeText(requireContext(), it, Toast.LENGTH_LONG).show()
+        }
+
+        val inputMethodManager =
+            requireContext().getSystemService(INPUT_METHOD_SERVICE) as? InputMethodManager
+
+        binding.clearButton.setOnClickListener {
+            binding.searchEditText.setText("")
+            inputMethodManager?.hideSoftInputFromWindow(binding.searchEditText.windowToken, 0)
+            viewModel.showHistory(binding.searchEditText.hasFocus(), "")
+        }
+
+        binding.clearHistoryButton.setOnClickListener {
+            viewModel.clearHistory()
+        }
+
+
+        searchTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                binding.clearButton.isVisible = !s.isNullOrEmpty()
+                if (!s.isNullOrEmpty()) hideSearchHistory()
+                viewModel.showHistory(binding.searchEditText.hasFocus(), s?.toString() ?: "")
+                viewModel.searchDebounce(s?.toString() ?: "")
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        }
+        binding.searchEditText.apply {
+            addTextChangedListener(searchTextWatcher)
+            setOnFocusChangeListener { _, hasFocus ->
+                viewModel.showHistory(hasFocus, this.text.toString())
+            }
+            requestFocus()
+            inputMethodManager?.showSoftInput(binding.searchEditText, InputMethodManager.SHOW_IMPLICIT)
+            setOnEditorActionListener { _, actionId, _ ->
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    viewModel.searchTracks(this.text.toString())
+                }
+                false
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding.searchEditText.removeTextChangedListener(searchTextWatcher)
+        _binding = null
+    }
+
+    private fun render(state: SearchState) {
+        when (state) {
+            is SearchState.Loading -> showLoadingState()
+            is SearchState.Error -> showErrorState(state.errorMessageId)
+            is SearchState.Empty -> showEmptyState(state.messageId)
+            is SearchState.Content -> showContentState(state.tracks)
+            is SearchState.History -> showHistoryState(state.tracks)
+            is SearchState.EmptyScreen -> showEmptyScreenState()
+        }
+    }
+
+    private fun showLoadingState() {
+        hidePlaceholder()
+        hideListOfTracks()
+        hideSearchHistory()
+        showProgressBar()
+    }
+
+    private fun showErrorState(placeholderTextId: Int) {
+        hideProgressBar()
+        hideListOfTracks()
+        hideSearchHistory()
+        showPlaceholder(getString(placeholderTextId))
+    }
+
+    private fun showEmptyState(placeholderTextId: Int) {
+        hideProgressBar()
+        hideListOfTracks()
+        hideSearchHistory()
+        showPlaceholder(getString(placeholderTextId))
+    }
+
+    private fun showContentState(foundTracks: List<TrackUi>) {
+        hideProgressBar()
+        hidePlaceholder()
+        hideSearchHistory()
+        showListOfTracks(foundTracks)
+    }
+
+    private fun showHistoryState(tracks: List<TrackUi>) {
+        hideProgressBar()
+        hidePlaceholder()
+        hideListOfTracks()
+        showSearchHistory(tracks)
+    }
+
+    private fun showEmptyScreenState() {
+        hideProgressBar()
+        hidePlaceholder()
+        hideListOfTracks()
+        hideSearchHistory()
+    }
+
+    private fun hideProgressBar() {
+        binding.progressBar.visibility = View.GONE
+    }
+
+    private fun showProgressBar() {
+        binding.progressBar.visibility = View.VISIBLE
+    }
+
+    private fun hideListOfTracks() {
+        binding.tracksList.visibility = View.GONE
+    }
+
+    private fun showListOfTracks(foundTracks: List<TrackUi>) {
+        adapter.tracks.clear()
+        adapter.tracks.addAll(foundTracks)
+        adapter.notifyDataSetChanged()
+        binding.tracksList.visibility = View.VISIBLE
+    }
+
+    private fun hidePlaceholder() {
+        binding.searchPlaceholder.visibility = View.GONE
+    }
+
+    private fun showPlaceholder(text: String) {
+        binding.searchPlaceholderText.text = text
+        if (text === getString(R.string.search_nothing)) {
+            binding.searchPlaceholderImage.setImageResource(R.drawable.search_nothing)
+            binding.updateButton.visibility = View.GONE
+        }
+        if (text === getString(R.string.search_error)) {
+            binding.searchPlaceholderImage.setImageResource(R.drawable.search_error)
+            binding.updateButton.visibility = View.VISIBLE
+        }
+        binding.searchPlaceholder.visibility = View.VISIBLE
+    }
+
+    private fun hideSearchHistory() {
+        binding.searchHistoryLayout.visibility = View.GONE
+    }
+
+    private fun showSearchHistory(listOfTracks: List<TrackUi>) {
+        searchHistoryAdapter.tracks.clear()
+        searchHistoryAdapter.tracks.addAll(listOfTracks)
+        searchHistoryAdapter.notifyDataSetChanged()
+        binding.searchHistoryLayout.visibility = View.VISIBLE
+    }
+
+    private fun clickDebounce(): Boolean {
+        val current = isClickAllowed
+        if (isClickAllowed) {
+            isClickAllowed = false
+            handler.postDelayed({ isClickAllowed = true }, CLICK_DEBOUNCE_DELAY)
+        }
+        return current
+    }
+
+    private fun onTrackClick(track: TrackUi) {
+        if (clickDebounce()) {
+
+            viewModel.addToHistory(track)
+            findNavController().navigate(R.id.action_searchFragment_to_playerFragment,
+                PlayerFragment.createArgs(track))
+        }
+    }
+
+    companion object {
+
+        const val CLICK_DEBOUNCE_DELAY = 1000L
+
+    }
+}
